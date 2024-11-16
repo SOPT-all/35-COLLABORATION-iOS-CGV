676e1700852d6d1d9d239a1ba36d4aacd3052040
mmaybei <yebbi2@ewhain.net>
2024-11-16 23:52:42 +0900

    [init] Initial Commit

---
+++ CGV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CGV
+//
+//  Created by 예삐 on 11/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
