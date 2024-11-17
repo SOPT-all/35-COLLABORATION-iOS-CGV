cda11594b4d61808865f6cd2dd20f58c7185f765
mmaybei <yebbi2@ewhain.net>
2024-11-17 16:15:22 +0900

    [hotfix/#5] 폴더링 변경

---
+++ CGV/Presentation/Seats/SeatsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SeatsViewController.swift
+//  CGV
+//
+//  Created by 예삐 on 11/17/24.
+//
+
+import UIKit
+
+final class SeatsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
