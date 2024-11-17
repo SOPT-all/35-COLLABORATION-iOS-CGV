4bd642dcd3b18c13410905f29e3c9e8512ce1bb1
mmaybei <yebbi2@ewhain.net>
2024-11-18 04:40:16 +0900

    [setting/#3] Base 파일 및 Util 코드 추가

---
+++ CGV/Resource/Base/BaseCollectionViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  BaseCollectionViewCell.swift
+//  CGV
+//
+//  Created by 예삐 on 11/18/24.
+//
+
+import UIKit
+
+import Then
+
+class BaseCollectionViewCell: UICollectionViewCell, ReuseIdentifiable {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setStyle()
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setStyle()
+        setUI()
+        setLayout()
+    }
+    
+    /// UI 컴포넌트 속성 설정 (do 메서드 관련)
+    func setStyle() {}
+    
+    /// UI 위계 설정 (addSubView 등)
+    func setUI() {}
+    
+    /// 오토레이아웃 설정 (SnapKit 코드 관련)
+    func setLayout() {}
+}
