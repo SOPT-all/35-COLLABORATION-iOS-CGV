fa766b24ea4e59555a136c92afc996378e6f3d40
ChoiAnYong <dksdyd78@naver.com>
2024-11-21 04:46:46 +0900

    [feat/#11] TimeBottomSheetViewController 구현

---
+++ CGV/Presentation/Time/TimeBottomSheet/TimeBottomSheetViewController.swift
@@ -0,0 +1,77 @@
+//
+//  TimeBottomSheetViewController.swift
+//  CGV
+//
+//  Created by 최안용 on 11/19/24.
+//
+
+import UIKit
+
+final class TimeBottomSheetViewController: BaseViewController {
+    private let rootView = TimeBottomSheetView()
+    
+    private var selectedTheaters: [TheaterInfo] = []
+    private var isValid: Bool = false
+    
+    override func loadView() {
+        view = rootView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func setupAction() {
+        super.setupAction()
+        
+        for button in rootView.theatersButtons {
+            button.addTarget(
+                self,
+                action: #selector(theaterButtonDidTap( _:)),
+                for: .touchUpInside
+            )
+        }
+        
+        rootView.selectButton.addTarget(
+            self,
+            action: #selector(selectButtonDidTap),
+            for: .touchUpInside
+        )
+    }
+}
+
+extension TimeBottomSheetViewController {
+    @objc
+    private func theaterButtonDidTap(_ sender: TheaterButton) {
+        guard let theater = sender.theater else { return }
+        if sender.isSelected {
+            guard let index = selectedTheaters.firstIndex(where: {
+                $0.theaterId == theater.theaterId
+            }) else { return }
+            
+            selectedTheaters.remove(at: index)
+            rootView.deleteTheaterChipButton(theater: theater.theaterName)
+        } else {
+            selectedTheaters.append(theater)
+            rootView.makeTheaterChipButton(theater: theater.theaterName)
+        }
+        checkValid()
+        sender.isSelected.toggle()
+    }
+    
+    @objc
+    private func selectButtonDidTap() {
+        dismiss(animated: true)
+    }
+    
+    private func checkValid() {
+        isValid = selectedTheaters.isEmpty
+        
+        rootView.selectButton.backgroundColor = isValid ? .cgvG500 : .cgvR400
+        rootView.selectButton.isEnabled = isValid ? false : true
+    }
+}
+
+#Preview {
+    TimeViewController()
+}
