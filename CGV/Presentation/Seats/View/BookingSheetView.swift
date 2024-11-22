c78a19f7a00e0e7f28263ad2a177b95f5df6d400
mmaybei <yebbi2@ewhain.net>
2024-11-22 11:54:29 +0900

    [feat/#24] 바텀시트 UI 구현

--- CGV/Presentation/Seats/View/BookingSheetView.swift
+++ CGV/Presentation/Seats/View/BookingSheetView.swift
@@ -30,6 +30,18 @@ final class BookingSheetView: BaseView {
     
     private let timeStackView = UIStackView()
     
+    private let guestCountLabel = UILabel()
+    
+    private let editCountButton = UIButton()
+    
+    private let priceStackView = UIStackView()
+    
+    private let priceLabel = UILabel()
+    
+    private let priceUnitLabel = UILabel()
+    
+    private let bookingButton = UIButton()
+    
     // MARK: - UISetting
     
     override func setStyle() {
@@ -85,14 +97,52 @@ final class BookingSheetView: BaseView {
             $0.isLayoutMarginsRelativeArrangement = true
             $0.layoutMargins = UIEdgeInsets(top: 6, left: 10, bottom: 6, right: 10)
         }
+        
+        guestCountLabel.do {
+            $0.setText("일반1", style: Kopub.body5, color: UIColor.cgvBlack)
+        }
+        
+        editCountButton.do {
+            $0.setTitle("인원변경", style: Kopub.body3, color: .cgvG600)
+            $0.setLayer(borderWidth: 1, borderColor: .cgvG600, cornerRadius: 8)
+        }
+        
+        priceStackView.do {
+            $0.axis = .horizontal
+            $0.spacing = 2
+            $0.alignment = .fill
+            $0.distribution = .fill
+        }
+        
+        priceLabel.do {
+            $0.setText("14,000", style: Kopub.head8, color: UIColor.cgvBlack)
+        }
+        
+        priceUnitLabel.do {
+            $0.setText("원", style: Kopub.head5, color: UIColor.cgvBlack, isSingleLine: true)
+        }
+        
+        bookingButton.do {
+            $0.setTitle("예매하기", style: Kopub.head7, color: .cgvWhite)
+            $0.backgroundColor = .cgvR400
+            $0.layer.cornerRadius = 12
+        }
     }
     
     override func setUI() {
-        addSubviews(titleLabel, infoStackView)
+        addSubviews(
+            titleLabel,
+            infoStackView,
+            guestCountLabel,
+            editCountButton,
+            priceStackView,
+            bookingButton
+        )
         infoStackView.addArrangedSubviews(dateStackView, theaterStackView, timeStackView)
         dateStackView.addArrangedSubview(dateLabel)
         theaterStackView.addArrangedSubview(theaterLabel)
         timeStackView.addArrangedSubview(timeLabel)
+        priceStackView.addArrangedSubviews(priceLabel, priceUnitLabel)
     }
     
     override func setLayout() {
@@ -104,6 +154,29 @@ final class BookingSheetView: BaseView {
         infoStackView.snp.makeConstraints {
             $0.top.equalTo(titleLabel.snp.bottom).offset(12)
             $0.centerX.equalToSuperview()
+        }
+        
+        guestCountLabel.snp.makeConstraints {
+            $0.top.equalTo(infoStackView.snp.bottom).offset(20)
+            $0.leading.equalToSuperview().inset(20)
+        }
+        
+        editCountButton.snp.makeConstraints {
+            $0.centerY.equalTo(guestCountLabel.snp.centerY)
+            $0.leading.equalTo(guestCountLabel.snp.trailing).offset(8)
+            $0.width.equalTo(66)
+            $0.height.equalTo(26)
+        }
+        
+        priceStackView.snp.makeConstraints {
+            $0.centerY.equalTo(guestCountLabel.snp.centerY)
+            $0.trailing.equalToSuperview().inset(20)
+        }
+        
+        bookingButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(36)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(60)
         }
     }
 }
