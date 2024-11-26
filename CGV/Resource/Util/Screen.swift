f0b8352c0891e975a2c2b130cb73ea0549f08f2a
mmaybei <yebbi2@ewhain.net>
2024-11-26 14:43:36 +0900

    [add/#27] screen 코드 추가 및 폰트 익스텐션 반영

---
+++ CGV/Resource/Util/Screen.swift
@@ -0,0 +1,22 @@
+//
+//  Screen.swift
+//  CGV
+//
+//  Created by 예삐 on 11/26/24.
+//
+
+import UIKit
+
+enum Screen {
+    static func width(_ value: CGFloat) -> CGFloat {
+        let screenWidth = UIScreen.main.bounds.width
+        let designWidth: CGFloat = 375.0
+        return screenWidth / designWidth * value
+    }
+    
+    static func height(_ value: CGFloat) -> CGFloat {
+        let screenHeight = UIScreen.main.bounds.height
+        let designHeight: CGFloat = 812.0
+        return screenHeight / designHeight * value
+    }
+}
