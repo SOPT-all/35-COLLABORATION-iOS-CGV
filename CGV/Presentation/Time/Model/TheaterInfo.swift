86f57908454360071d31097fcac51b2c9c50694a
ChoiAnYong <dksdyd78@naver.com>
2024-12-01 14:54:19 +0900

    [fix/#39] TheaterInfo 수정

--- CGV/Presentation/Time/Model/TheaterInfo.swift
+++ CGV/Presentation/Time/Model/TheaterInfo.swift
@@ -10,4 +10,36 @@ import Foundation
 struct TheaterInfo {
     let theaterName: String
     let theaterId: Int
+    var auditoriumType: [(String, String)] {
+        if theaterId == 1 {
+            return [
+                ("1관", "2D"),
+                ("1관", "2D"),
+                ("1관", "2D"),
+                ("1관", "2D"),
+                ("1관", "2D"),
+                ("1관", "2D")
+            ]
+        } else if theaterId == 2 {
+            return [
+                ("1관", "IMAX"),
+                ("1관", "IMAX"),
+                ("1관", "IMAX"),
+                ("1관", "IMAX"),
+                ("2관", "2D"),
+                ("2관", "2D")
+            ]
+        } else {
+            return [
+                ("1관", "IMAX"),
+                ("1관", "IMAX"),
+                ("1관", "IMAX"),
+                ("1관", "IMAX"),
+                ("2관", "2D"),
+                ("2관", "2D")
+            ]
+        }
+    }
 }
+
+
