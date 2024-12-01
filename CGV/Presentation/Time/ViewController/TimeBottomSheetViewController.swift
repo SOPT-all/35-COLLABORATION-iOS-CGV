f38dfc9e23a5af05941ae6f37ec6ad40ce9a19db
ChoiAnYong <dksdyd78@naver.com>
2024-12-01 14:57:27 +0900

    [fix/#39] TimeBottomSheetViewController 수정

--- CGV/Presentation/Time/ViewController/TimeBottomSheetViewController.swift
+++ CGV/Presentation/Time/ViewController/TimeBottomSheetViewController.swift
@@ -7,14 +7,20 @@
 
 import UIKit
 
+protocol TimeBottomSheetViewControllerDelegate: AnyObject {
+    func didTappedSelect(_ theaters: [TheaterInfo])
+}
+
 final class TimeBottomSheetViewController: BaseViewController {
     
     // MARK: - Property
-    
+        
     private let rootView = TimeBottomSheetView()
-    
-    var selectedTheaters: [TheaterInfo] = []
     private var isValid: Bool = false
+    
+    var theaterService: TheaterService?
+    var selectedTheaters: [TheaterInfo] = []
+    weak var delegate: TimeBottomSheetViewControllerDelegate?
     
     // MARK: - LifeCycle
     
@@ -23,19 +29,19 @@ final class TimeBottomSheetViewController: BaseViewController {
     }
 
     override func setupAction() {
-        for button in rootView.theatersButtons {
-            button.addTarget(
-                self,
-                action: #selector(theaterButtonDidTap( _:)),
-                for: .touchUpInside
-            )
-        }
-        
         rootView.selectButton.addTarget(
             self,
             action: #selector(selectButtonDidTap),
             for: .touchUpInside
         )
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        fetchTheaters()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        delegate?.didTappedSelect(selectedTheaters)
     }
 }
 
@@ -49,26 +55,79 @@ extension TimeBottomSheetViewController {
         rootView.selectButton.isEnabled = isValid ? false : true
     }
     
+    private func fetchTheaters() {
+        theaterService?.fetchMovieTheater(request: EmptyModel()) { [weak self] result in
+            switch result {
+            case .success(let response):
+                self?.rootView.makeTheaterButton(
+                    theaters: response.data,
+                    target: self,
+                    action: #selector(self?.theaterButtonDidTap(_:))
+                )
+            case .failure, .decodedError, .serverError, .networkFail:
+                print("실패")
+            }
+        }
+    }
+    
     @objc
     private func theaterButtonDidTap(_ sender: TheaterButton) {
         guard let theater = sender.theater else { return }
+        
         if sender.isSelected {
             guard let index = selectedTheaters.firstIndex(where: {
                 $0.theaterId == theater.theaterId
             }) else { return }
             
             selectedTheaters.remove(at: index)
-            rootView.deleteTheaterChipButton(theater: theater.theaterName)
+            rootView.deleteTheaterChipButton(theater: theater)
         } else {
             selectedTheaters.append(theater)
-            rootView.makeTheaterChipButton(theater: theater.theaterName)
+            rootView.makeTheaterChipButton(
+                theater: theater,
+                target: self,
+                action: #selector(self.theaterChipButtonDidTap(_:))
+            )
         }
         checkValid()
         sender.isSelected.toggle()
     }
     
     @objc
+    private func theaterChipButtonDidTap(_ sender: TheaterChipButton) {
+        guard let theater = sender.theater else { return }
+        guard let index = selectedTheaters.firstIndex(where: {
+            $0.theaterId == theater.theaterId
+        }) else { return }
+        
+        selectedTheaters.remove(at: index)
+        rootView.deleteTheaterChipButton(theater: theater)
+        changeTheaterButtonState(theater: theater)
+        checkValid()
+    }
+    
+    @objc
     private func selectButtonDidTap() {
         dismiss(animated: true)
+    }
+    
+    private func changeTheaterButtonState(theater: TheaterInfo) {
+        for subview in rootView.recentStackView.subviews {
+            if let button = subview as? TheaterButton {
+                if button.theater?.theaterId == theater.theaterId {
+                    button.isSelected = false
+                    break
+                }
+            }
+        }
+        
+        for subview in rootView.surroundingStackView.subviews {
+            if let button = subview as? TheaterButton {
+                if button.theater?.theaterId == theater.theaterId {
+                    button.isSelected = false
+                    break
+                }
+            }
+        }
     }
 }
